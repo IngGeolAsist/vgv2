2914dfa2edcfcbf045190d60d2ca6f6760b417cb
IngGeolAsist <80356680+IngGeolAsist@users.noreply.github.com>
2022-03-07 11:28:38 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Hd-UQYwqcOBUGBBxTcgS9xRhL2fdAuZ2swh9Jr02G4g/edit#gid=957210929';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1o5tUQR8Oi_oXikhQ0zIhw-BO_sJ0VccztjbGrQ4cD_s/edit#gid=957210929';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBOhOxQmDduXiOjls9aOZ4VmNM4Sx2K-Vw';
